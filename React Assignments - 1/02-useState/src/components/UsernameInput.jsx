2c03af140ce9d9f38178c1312a4c8461c397beb5
Your-Shivam <shivamkumar1019988@gmail.com>
2024-05-20 16:35:42 +0530

    useState

---
+++ React Assignments - 1/02-useState/src/components/UsernameInput.jsx
@@ -0,0 +1,23 @@
+// src/components/UsernameInput.jsx
+
+import { useState } from "react";
+
+function UsernameInput() {
+  const [username, setUsername] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value)
+          /* complete the missing code */
+        }}
+      />
+      <p>Username: {username.toLowerCase()}</p>
+    </div>
+  );
+}
+
+export default UsernameInput;
